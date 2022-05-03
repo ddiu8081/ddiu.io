d304b49482956717553aff43b963d9f25d59dc0b
ddiu8081 <ddiu8081@gmail.com>
2022-05-03 20:16:35 +0800

    fix: dark mode when first load

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,6 @@ import Posts from './components/Posts'
 import Footer from './components/Footer'
 
 const App: Component = () => {
-  const colorSchema  = window.matchMedia('(prefers-color-scheme: dark)')
-    
-  colorSchema.addEventListener('change', () => {
-    if (colorSchema.matches) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-  })
-
   return (
     <main class="max-w-[65ch] px-6 py-[8vh] mx-auto prose prose-truegray xl:text-lg dark:prose-invert">
       <Hero />
--- src/main.tsx
+++ src/main.tsx
@@ -6,4 +6,15 @@ import './assets/font.css'
 
 import App from './App'
 
+const colorSchema = window.matchMedia('(prefers-color-scheme: dark)')
+const performDark = () => {
+  if (colorSchema.matches) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+}
+colorSchema.addEventListener('change', performDark)
+performDark()
+
 render(() => <App />, document.getElementById('root') as HTMLElement)
