e51fce9e82a981e78a4e954e70ce4dd447d7c830
ddiu8081 <ddiu8081@gmail.com>
2022-05-22 01:20:00 +0800

    feat(ui): responsive

--- pages/index/_components/Hero.tsx
+++ pages/index/_components/Hero.tsx
@@ -5,12 +5,12 @@ export default () => {
   // const helloWord = helloWordArr[Math.floor((Math.random() * helloWordArr.length))]
   
   return (
-    <header mt-18 class="outfit">
-      <h1 text-6xl font-bold class="not-prose">
+    <header class="outfit mt-12 md:mt-18">
+      <h1 class="text-5xl font-bold md:text-6xl">
         <span class="block">Hello, </span>
         <span class="block mt-2">I'm Diu.</span>
       </h1>
-      <div mt-6>
+      <div class="mt-6 text-base md:text-xl">
         <div>
           <span>Front-end & iOS developer / Amateur designer.</span>
         </div>
