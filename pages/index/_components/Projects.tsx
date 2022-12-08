fba6266add2b5b6b98c2405254de22ca5750bbc8
ddiu8081 <ddiu8081@gmail.com>
2022-12-09 00:22:11 +0800

    feat: use fluent-emoji-flat

--- pages/index/_components/Projects.tsx
+++ pages/index/_components/Projects.tsx
@@ -7,52 +7,52 @@ export default () => {
       name: 'Moegi Theme for VS Code',
       desc: 'An elegant theme for VS Code.',
       link: 'https://github.com/moegi-design/vscode-theme',
-      icon: 'i-fluent-emoji-cherry-blossom',
+      icon: 'i-fluent-emoji-flat-cherry-blossom',
     }, {
       name: 'bilicli',
       desc: 'Bilibili-live danmu dashboard in your terminal.',
       link: 'https://github.com/ddiu8081/bilicli',
-      icon: 'i-fluent-emoji-control-knobs',
+      icon: 'i-fluent-emoji-flat-control-knobs',
     }, {
       name: 'blive-message-listener',
       desc: 'Bilibili-live danmu listener with type.',
       link: 'https://github.com/ddiu8081/blive-message-listener',
-      icon: 'i-fluent-emoji-keyboard',
+      icon: 'i-fluent-emoji-flat-keyboard',
     }, {
       name: 'bus-vis',
       desc: 'City bus visualizations of China',
       link: 'https://bus.ljl.li/',
-      icon: 'i-fluent-emoji-oncoming-bus',
+      icon: 'i-fluent-emoji-flat-oncoming-bus',
     }, {
       name: 'Tin',
       desc: 'Mathematical arts driven by t, i and anything',
       link: 'https://tin.ddiu.io/',
-      icon: 'i-fluent-emoji-eight-spoked-asterisk',
+      icon: 'i-fluent-emoji-flat-eight-spoked-asterisk',
     }, {
       name: 'oreooo',
       desc: 'An oreo generater',
       link: 'https://oreo.ddiu.io/',
-      icon: 'i-fluent-emoji-cookie',
+      icon: 'i-fluent-emoji-flat-cookie',
     }, {
       name: 'Fluent emoji maker',
       desc: 'Generate your own Fluent Emojis!',
       link: 'https://emoji.ddiu.io/',
-      icon: 'i-fluent-emoji-rolling-on-the-floor-laughing',
+      icon: 'i-fluent-emoji-flat-rolling-on-the-floor-laughing',
     }, {
       name: 'placeholder-image',
       desc: 'Generate placeholder images by simply adding params',
       link: 'https://ph.ljl.li/',
-      icon: 'i-fluent-emoji-ice',
+      icon: 'i-fluent-emoji-flat-ice',
     }, {
       name: 'Ghost theme Moegi',
       desc: 'An elegant & fresh ghost theme',
       link: 'https://github.com/moegi-design/ghost-theme-Moegi',
-      icon: 'i-fluent-emoji-newspaper',
+      icon: 'i-fluent-emoji-flat-newspaper',
     }, {
       name: 'Diu\'s Online Riddle',
       desc: 'An online puzzle game',
       link: 'https://riddle.ddiu.me/',
-      icon: 'i-fluent-emoji-exploding-head',
+      icon: 'i-fluent-emoji-flat-exploding-head',
     },
   ]
   const openLink = () => {
